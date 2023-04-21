9138e7a596a4b502b060a591c124de9215109dc4
oumaymabid <abid.oumayma@gmail.com>
2023-04-21 16:00:55 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *Description: prints all the numbers of base 16 in lowercase
+  *Return: O (success)
+  */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; n++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
