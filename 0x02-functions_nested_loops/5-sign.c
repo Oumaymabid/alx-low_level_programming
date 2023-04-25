f5ec6e1efbd9a45a0a26ebc5605b43c635eca286
oumaymabid <abid.oumayma@gmail.com>
2023-04-25 11:20:44 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,11 +7,11 @@
   *Return: 0 and prints 0 if n is zero
   *Retunr: -1 and prints - if n is less than zero
  */
-int print_sign(int n);
+int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		return (1);
 	} else if (n == 0)
 	{
@@ -19,7 +19,7 @@ int print_sign(int n);
 		return (0);
 	} else if (n < 0)
 	{
-		_putchar(45);
+		_putchar('-');
 	}
 		return (-1);
 }
