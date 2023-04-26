eb4bc0ce66c9f9927b800ab16a36fe214aec5276
oumaymabid <abid.oumayma@gmail.com>
2023-04-26 05:19:27 -0700

    0-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+  *positive_or_negative - print whether the variable i is positive or negative
+  *@i: the char to be checked
+  *Return: if the number is greater than 0: is positive
+  *if the number is 0: is zero
+  *if the number is less than 0: is negative
+ */
 void positive_or_negative(int i)
 {
 	if (i > 0)
