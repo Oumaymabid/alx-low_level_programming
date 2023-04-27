68d920c3b87bfe9445df7f120ffc856919352ada
oumaymabid <abid.oumayma@gmail.com>
2023-04-27 07:08:35 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  *_isdigit - checks digits between 0 and 9
+  *@c: the chart to be checked
+  *Return: 1 or 0
+  */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		print (1);
+	else
+		print (0);
+}
+
