a35dff05f917fdfd12fa45f3c66e63b8bda3e605
oumaymabid <abid.oumayma@gmail.com>
2023-05-12 08:00:10 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *main - prints all arguments it receives.
+  *@argc: count of arguments
+  *@argv: vectors of arguments
+  *Return: 0 success
+  */
+int main(int argc, char *argv[])
+{
+	int j;
+
+	for (j = 0; j < argc; j++)
+	{
+		printf("%s ", argv[j]);
+	}
+	printf("\n");
+	return (0);
+}
