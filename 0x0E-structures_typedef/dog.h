e6f721ddd688224d18ac650534453d2c3ea068d0
oumaymabid <abid.oumayma@gmail.com>
2023-05-22 09:29:54 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  *struct dog - info about the dog
+  *@name: input
+  *@age: input
+  *@owner: input
+  *Description: longer desc
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
