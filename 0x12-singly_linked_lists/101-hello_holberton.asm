1a7a4cb317c70e780cab5393b863815d50366f94
oumaymabid <abid.oumayma@gmail.com>
2023-06-01 09:57:28 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,13 @@
-global    main
-          extern    printf
+section .data
+text: db "Hello, Holberton", 10
+
+section .text
+global main
+extern printf
+
 main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov 	eax, 0
-	  ret
-format: db `Hello, Holberton\n`,0
+	mov   edi, format
+	xor   eax, eax
+	call  printf
+	mov 	eax, 0
+	ret
