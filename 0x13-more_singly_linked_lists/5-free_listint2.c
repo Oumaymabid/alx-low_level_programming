db703b65a419c4d71a8e27ba43c75d37dabb4ff8
oumaymabid <abid.oumayma@gmail.com>
2023-06-05 08:28:04 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+  *free_listint2 -  frees a listint_t list
+  *@head: input
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *t;
+
+	if (head == NULL)
+	return;
+
+	while (*head)
+	{
+		t = (*head)->next;
+		free(*head);
+		*head = t;
+	}
+	*head = NULL;
+}
